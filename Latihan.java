efc0bc594ba9e72cc8cfe3ad063e237bf0ca106b
euklideshutasoit <euklidesmarimaipa3@gmail.com>
2020-01-15 16:00:17 +0700

    first

---
+++ Latihan.java
@@ -0,0 +1,10 @@
+public class Latihan {
+    
+    public static void main(String[]args){
+        String nama = "Euklides Marima";
+        String alamat = "cibubur";
+        String kelas = "3AEU2";
+        int umur = 18;
+        
+    System.out.println("Nama :" + nama + "\nAlamat :" + alamat + "\nKelas :" + kelas + "\nUmur :" + umur);
+}
